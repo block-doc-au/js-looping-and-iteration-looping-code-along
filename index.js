aea5bbf8a851730a2b55ec7fdb5487b8e247e1ed
block-doc-au <neilc@cns-gc.com.au>
2022-08-19 12:47:58 +1000

    NC completed loops and iteraiton lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+
+function writeCards (names, event){
+    let myArray = new Array;
+ for (let i=0; i<names.length; i++){
+    myArray[i] =`Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+ }
+ return myArray;
+};
+
+function countDown (startNumber){
+    let i = startNumber;
+    while (i > -1) {
+        console.log(i);
+        i --;
+    };
+}
